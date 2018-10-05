c5bfec64e1f6b30a2cdd008a4bb9829c4e0b1435
wang shijie <wangshijie@wangs-MacBook-Pro.local>
2018-10-06 01:29:55 +0800

    新增清单列表页

--- checklists/checklists/AllListsViewController.swift
+++ checklists/checklists/AllListsViewController.swift
@@ -8,15 +8,15 @@
 
 import UIKit
 
-class AllListsViewController: UITableViewController {
+class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate {
     var lists = [Checklist]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         navigationController?.navigationBar.prefersLargeTitles = true
@@ -30,14 +30,14 @@ class AllListsViewController: UITableViewController {
         list = Checklist(name: "To Do")
         lists.append(list)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return lists.count
@@ -53,13 +53,13 @@ class AllListsViewController: UITableViewController {
                                    reuseIdentifier: cellIdentifier)
         }
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = makeCell(for: tableView)
         let checklist = lists[indexPath.row]
         cell.textLabel!.text = checklist.name
         cell.accessoryType = .detailDisclosureButton
-
+        
         return cell
     }
     
@@ -69,13 +69,53 @@ class AllListsViewController: UITableViewController {
         performSegue(withIdentifier: "ShowChecklist", sender: checklist)
     }
     
+    override func tableView(
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCellEditingStyle,
+        forRowAt indexPath: IndexPath) {
+        lists.remove(at: indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue,
                           sender: Any?) {
         if segue.identifier == "ShowChecklist" {
             let controller = segue.destination
                 as! ChecklistViewController
             controller.checklist = sender as! Checklist
+        } else if segue.identifier == "AddChecklist" {
+            let controller = segue.destination
+                as! ListDetailViewController
+            controller.delegate = self
         }
     }
-
+    
+    // MARK:- List Detail View Controller Delegates
+    func listDetailViewControllerDidCancel(
+        _ controller: ListDetailViewController) {
+        navigationController?.popViewController(animated: true)
+    }
+    func listDetailViewController(
+        _ controller: ListDetailViewController,
+        didFinishAdding checklist: Checklist) {
+        let newRowIndex = lists.count
+        lists.append(checklist)
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+        navigationController?.popViewController(animated: true)
+    }
+    func listDetailViewController(
+        _ controller: ListDetailViewController,
+        didFinishEditing checklist: Checklist) {
+        if let index = lists.index(of: checklist) {
+            let indexPath = IndexPath(row: index, section: 0)
+            if let cell = tableView.cellForRow(at: indexPath) {
+                cell.textLabel!.text = checklist.name
+            }
+        }
+        navigationController?.popViewController(animated: true)
+    }
 }
